e46f4da0e18db1b910026a236757dc22fd473cd1
santitomatis <santiagotomatis06@gmail.com>
2022-06-16 13:01:14 -0300

    Created the basic structure

---
+++ password_generator.py
@@ -0,0 +1,12 @@
+import random
+
+def password_generator():
+    pass
+
+def run():
+    password = password_generator()
+    print("Your random secure password is: " + password)
+
+if __name__ == "__main__":
+    run()
+    
