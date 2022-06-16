744a67b2863f92cb3744a113fa70bd23ea017773
santitomatis <santiagotomatis06@gmail.com>
2022-06-16 13:02:26 -0300

    started coding the password generating function but it throws an error

--- password_generator.py
+++ password_generator.py
@@ -1,7 +1,21 @@
 import random
 
 def password_generator():
-    pass
+    UPPER = ('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'Ñ', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'X', 'Y', 'Z')
+    LOWER = ('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'ñ', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'x', 'y', 'z')
+    NUMS = ('1', '2', '3', '4', '5', '6', '7', '8', '9', '0')
+    CHARS = ('*', '+', '-', '/', '@', '_', '?', '!', '[', '{', '(', ')', '}', ']', ',', ';', '.', '>', '<', '~', '°', '^', '&', '$', '#', '"')
+
+    string = UPPER + LOWER + NUMS + CHARS
+
+    password = []
+
+    for i in range(16):
+        random_character = random(string)
+        password.append(random_character)
+
+    password = "".join(password)
+    return password
 
 def run():
     password = password_generator()
